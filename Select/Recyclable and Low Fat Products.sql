56b9e91b7f801fd58119d0021c34c0b9dd9c72a6
sepsoleimanian <sepsoleimanian@gmail.com>
2025-05-03 12:46:56 +0330

    second question

--- Select/Recyclable and Low Fat Products.sql
+++
@@ -1,4 +0,0 @@
--- Link: https://leetcode.com/problems/recyclable-and-low-fat-products/?envType=study-plan-v2&envId=top-sql-50
-SELECT product_id
-FROM products
-WHERE low_fats = 'Y' AND recyclable = 'Y'
